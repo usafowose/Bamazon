8ac202b9470b92de314c983f1b5f29cdbdc1d35c
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2019-11-14 14:55:48 -0500

    Changed host from local to Winows sql host connection and established connection to DB

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,30 +1,41 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 
-inquirer.prompt([
-    {
-        type: "input",
-        message: "How old are you",
-        name: "Andrew",
+var connection = mysql.createConnection({
+    host: "192.168.99.100",
+    port: "3306",
+    user: "root",
+    password: "docker",
+    database: "bamazon"
+})
 
-    },
-    {
-        type: "input",
-        message: "How old are you",
-        name: "Andrew",
+connection.connect(function (err, res) {
+    if (err) {
+        throw err;
+    }
+    else {
+        console.log("Connection Made");
+    }
+});
 
-    },
-    {
-        type: "input",
-        message: "How old are you",
-        name: "Andrew",
+// inquirer.prompt([
+//     {
+//         type: "input",
+//         message: "What product (ID#) would you like to buy?",
+//         name: "Department ID:",
 
-    },
+//     },
+//     {
+//         type: "input",
+//         message: "How many would you like to buy?",
+//         name: "Quantity",
 
-])
-    .then(function (answers) {
-        console.log('Bamazon Customer Loaded');
-        console.log(answers);
-    })
+//     },
+
+// ])
+//     .then(function (answers) {
+//         console.log('Bamazon Customer Loaded');
+//         console.log(answers);
+//     })
 
 
